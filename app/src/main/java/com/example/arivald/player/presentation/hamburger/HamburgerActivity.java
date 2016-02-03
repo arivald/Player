4e55840ff3a7c2c43ebcb843123b415bc8651934
Arivald <arivald@interia.pl>
2016-02-03 12:48:17 +0100

    push test 2

--- app/src/main/java/com/example/arivald/player/presentation/hamburger/HamburgerActivity.java
+++ app/src/main/java/com/example/arivald/player/presentation/hamburger/HamburgerActivity.java
@@ -52,7 +52,6 @@ public class HamburgerActivity extends BaseActivity
 //    @Bind(R.id.nav_header_song_path)
 //    TextView navigationSongPath;
 //
-    
 //    @Bind(R.id.nav_header_mc_prior)
 //    TextView navigationPriorSong;
 //
